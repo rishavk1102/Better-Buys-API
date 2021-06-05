1638ede4eeaa98b852cc07bb3b26bbebb230c352
rishavk1102 <rishavk1102@gmail.com>
2021-06-05 17:56:49 +0530

    craeted product model

---
+++ models/Products.php
@@ -0,0 +1,58 @@
+<?php
+
+$ds = DIRECTORY_SEPARATOR;
+$base_dir = realpath(dirname(__FILE__). $ds . '..') . $ds;
+
+require_once("{$base_dir}includes{$ds}Database.php"); // Including database
+
+// class start
+class Product
+{
+    private $table = 'products';
+
+    public $id;
+    public $seller_id;
+    public $name;
+    public $image;
+    public $price_per_kg;
+    public $description;
+    public $interaction_count;
+
+    // contructor
+    public function __construct()
+    {
+    }
+ 
+    // validating if params exists or not
+    public function validate_params($value)
+    {
+        return (!empty($value));
+    }
+
+    public function add_product()
+    {
+        global $database;
+
+        $this->seller_id = trim(htmlspecialchars(strip_tags($this->seller_id)));
+        $this->name = trim(htmlspecialchars(strip_tags($this->name)));
+        $this->image = trim(htmlspecialchars(strip_tags($this->image)));
+        $this->price_per_kg = trim(htmlspecialchars(strip_tags($this->price_per_kg)));
+        $this->description = trim(htmlspecialchars(strip_tags($this->description)));
+
+        $sql = "INSERT INTO $this->table (seller_id, name, image, price_per_kg, description) VALUES (
+            '" .$database->escape_value($this->seller_id). "',
+            '" .$database->escape_value($this->name). "',
+            '" .$database->escape_value($this->image). "',
+            '" .$database->escape_value($this->price_per_kg). "',
+            '" .$database->escape_value($this->description). "'
+        )";
+
+        $result = $database->query($sql);
+
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
